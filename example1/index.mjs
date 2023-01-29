8c62b8d668369dfdc792755a541a67142ee8dac3
Vitali Lapeka <lapeko88@gmail.com>
2023-01-29 11:23:17 +0100

    revert index.mjs

--- example1/index.mjs
+++ example1/index.mjs
@@ -3,8 +3,6 @@ import express from "express";
 const PORT = process.env.PORT || "3000";
 const app = express();
 
-// some comment 1
-
 app.get("/", (req, res) => res.end("Hello world !"));
 
 app.listen(PORT, () => console.log("App is running on port:", PORT));
