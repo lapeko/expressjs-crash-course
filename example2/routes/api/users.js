5513c4dcf45d9a1930227cae0c12926ec5827dfc
Vitali Lapeka <lapeko88@gmail.com>
2023-01-29 16:59:09 +0100

    add update user

--- example2/routes/api/users.js
+++ example2/routes/api/users.js
@@ -4,14 +4,29 @@ const users = require("../../users");
 const router = express.Router();
 
 router.get("/", (req, res) => res.json({ data: users }));
+
 router.get("/:id", (req, res) => {
   const user = users.filter(({ id }) => req.params.id == id)[0];
   if (!user) return res.status(404).json({ data: null });
   res.json({ data: user });
 });
+
 router.post("/", (req, res) => {
-  users.push({ id: users[users.length - 1].id + 1, ...req.body });
-  res.send(users);
+  users.push({
+    id: users[users.length - 1].id + 1,
+    ...req.body,
+    status: "active",
+  });
+  res.send({ data: users });
+});
+
+router.put("/:id", (req, res) => {
+  const user = users.filter(({ id }) => req.params.id == id)[0];
+  if (!user) return res.status(404).send({ data: null });
+  const { name, email } = req.body;
+  user.name = name;
+  user.email = email;
+  res.send({ data: user });
 });
 
 module.exports = router;
