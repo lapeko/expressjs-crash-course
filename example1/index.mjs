89bc079fc1f1ef4bd1bfc027c98b12b66883b1d4
Vitali Lapeka <lapeko88@gmail.com>
2023-01-29 11:30:20 +0100

    test commit to revert it later

--- example1/index.mjs
+++ example1/index.mjs
@@ -1,6 +1,9 @@
 import express from "express";
 
 const PORT = process.env.PORT || "3000";
+
+// comment to revert it later
+
 const app = express();
 
 app.get("/", (req, res) => res.end("Hello world !"));
