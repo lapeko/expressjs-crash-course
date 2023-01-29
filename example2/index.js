d1e327786ec5ac1ddbcbb3df3d20950d78aa700f
Vitali Lapeka <lapeko88@gmail.com>
2023-01-29 16:05:35 +0100

    small refactoring

--- example2/index.js
+++ example2/index.js
@@ -9,10 +9,7 @@ app.use(logger);
 app.get("/api/users", (req, res) => res.json({ data: users }));
 app.get("/api/users/:id", (req, res) => {
   const user = users.filter(({ id }) => req.params.id == id)[0];
-  if (!user) {
-    res.status(404);
-    return res.json({ data: null });
-  }
+  if (!user) return res.status(404).json({ data: null });
   res.json({ data: user });
 });
 
