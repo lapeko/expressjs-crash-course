65d4304cecf5b0ce2410b2b8fa49839c66998e62
Vitali Lapeka <lapeko88@gmail.com>
2023-01-29 10:53:19 +0100

    add simple express server

---
+++ example1/index.mjs
@@ -0,0 +1,8 @@
+import express from "express";
+
+const PORT = process.env.PORT || "3000";
+const app = express();
+
+app.get("/", (req, res) => res.end("Hello world !"));
+
+app.listen(PORT, () => console.log("App is running on port:", PORT));
